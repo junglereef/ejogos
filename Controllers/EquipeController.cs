6e27a94f2db32a0fe363ed2e41f25d9c26412418
junglereef <atila.iglesias@hotmail.com>
2022-05-11 18:47:34 -0300

    Aula 11

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,4 +1,5 @@
 ﻿using EJogos.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EJogos.Controllers
@@ -23,6 +24,23 @@ namespace EJogos.Controllers
             ViewBag.Equipes = equipeModel.LerEquipes();
 
             return View();
+        }
+
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            
+            Equipe novaEquipe = new Equipe();
+
+            novaEquipe.idEquipe = int.Parse(form["IdEquipe"]);
+            novaEquipe.Nome = form["Nome"];
+            novaEquipe.Imagem = form["Imagem"];
+
+            equipeModel.Criar(novaEquipe);
+
+            ViewBag.Equipes = equipeModel.LerEquipes();
+
+            return LocalRedirect("~/Equipe");
+
         }
     }
 }
