90e0d604362832ba7864b7726135543399fe56e6
junglereef <atila.iglesias@hotmail.com>
2022-05-11 15:40:22 -0300

    Created ErrorViewModel.cs

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EJogos.Models
+
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
