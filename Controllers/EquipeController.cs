90aa720ddd1610161bae0d1568b5165348d981a1
junglereef <atila.iglesias@hotmail.com>
2022-05-11 12:52:59 -0300

    Criação Repositorio

---
+++ Controllers/EquipeController.cs
@@ -0,0 +1,28 @@
+﻿using EJogos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EJogos.Controllers
+{
+    public class EquipeController : Controller
+    {
+
+        //ActionResult representam os varios codigos HTTP,
+
+        //Codigos HTTP - https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
+
+
+        Equipe equipeModel = new Equipe();
+
+        public IActionResult Index()
+        {
+            //ViewBag = Reserva de espaco para armazenar informacoes para recuparar na VIEW.
+
+            //ViewBag = tem a função de "carregar" as informaçoes do Controller para a 
+            // VIEW.
+
+            ViewBag.Equipes = equipeModel.LerEquipes();
+
+            return View();
+        }
+    }
+}
---
+++ Interface/IEquipe.cs
@@ -0,0 +1,17 @@
+﻿using EJogos.Models;
+using System.Collections.Generic;
+
+namespace EJogos.Interface
+{
+    public interface IEquipe
+    {
+        //Contrato
+        //Representa os métodos que são obrigatoris em
+        // em uma classe.
+
+        void Criar(Equipe novaequipe);
+        List<Equipe> LerEquipes();
+
+
+    }
+}
